615fea506f1b5f1c15b6b2e8afd378c7beb2752a
David Johnston <david@blacksheepcode.com>
2020-05-23 11:39:51 +1000

    eslint config working

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 11,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'arrow-body-style': ['off'], 
+    'arrow-parens': ['warn', 'always'],
+    'indent': [
+      'warn',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'warn',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};
--- src/original.js
+++ src/original.js
@@ -1,54 +1,55 @@
+/* eslint-disable no-unused-vars */
 
 
-// // Arrow functions - Creating
+// Arrow functions - Creating
 
-// // Curly bracket around body
-// const af1 = () => "hello"; 
-// const af2 = () => {
-//     return "hello"; 
-// }; 
+// Curly bracket around body
+const af1 = () => 'hello'; 
+const af2 = () => {
+  return 'hello'; 
+}; 
 
-// // Round brackets around single param
-// const af3 = value => "hello"; 
-// const af4 = (value) => "hello"; 
+// Round brackets around single param
+const af3 = value => value; 
+const af4 = (value) => value; 
 
 
-// // Two params
-// const af5 = (foo, bar) => "hello"; 
-// const af6 = (
-//     foo, 
-//     bar,
-// ) => "hello"; 
+// Two params
+const af5 = (foo, bar) => 'hello'; 
+const af6 = (
+  foo, 
+  bar,
+) => 'hello'; 
 
-// // Four params
-// const af7 = (foo, bar, biz, bash) => "hello"; 
-// const af8 = (
-//     foo, 
-//     bar, 
-//     biz, 
-//     bash
-// ) => "hello"; 
+// Four params
+const af7 = (foo, bar, biz, bash) => 'hello'; 
+const af8 = (
+  foo, 
+  bar, 
+  biz, 
+  bash
+) => 'hello'; 
 
-// // Arrow functions - Creating
+// Arrow functions - Creating
 
-// // One arg
-// af3("hello");
+// One arg
+af3('hello');
 
-// // Two args
-// af5("hello", "world"); 
-// af5(
-//     "hello",
-//     "world"
-// );
+// Two args
+af5('hello', 'world'); 
+af5(
+  'hello',
+  'world'
+);
 
-// // Four args
-// af7("one", "two", "three", "four");
-// af7(
-//     "one",
-//     "two",
-//     "three",
-//     "four"
-// );
+// Four args
+af7('one', 'two', 'three', 'four');
+af7(
+  'one',
+  'two',
+  'three',
+  'four'
+);
 
 
 
