4c73c894392813f00613e381babde627da495cda
David Johnston <david@blacksheepcode.com>
2020-05-23 11:27:41 +1000

    Park chagnes

---
+++ src/a.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "node": true
+//     }, 
+//     "plugins": [
+//         "import"
+//     ], 
+//     "rules" : {
+//         "arrow-body-style": ["off"], 
+//         "arrow-parens": ["warn", "always"]
+//     }
+// }
---
+++ src/original.js
@@ -0,0 +1,56 @@
+
+
+// // Arrow functions - Creating
+
+// // Curly bracket around body
+// const af1 = () => "hello"; 
+// const af2 = () => {
+//     return "hello"; 
+// }; 
+
+// // Round brackets around single param
+// const af3 = value => "hello"; 
+// const af4 = (value) => "hello"; 
+
+
+// // Two params
+// const af5 = (foo, bar) => "hello"; 
+// const af6 = (
+//     foo, 
+//     bar,
+// ) => "hello"; 
+
+// // Four params
+// const af7 = (foo, bar, biz, bash) => "hello"; 
+// const af8 = (
+//     foo, 
+//     bar, 
+//     biz, 
+//     bash
+// ) => "hello"; 
+
+// // Arrow functions - Creating
+
+// // One arg
+// af3("hello");
+
+// // Two args
+// af5("hello", "world"); 
+// af5(
+//     "hello",
+//     "world"
+// );
+
+// // Four args
+// af7("one", "two", "three", "four");
+// af7(
+//     "one",
+//     "two",
+//     "three",
+//     "four"
+// );
+
+
+
+
+
